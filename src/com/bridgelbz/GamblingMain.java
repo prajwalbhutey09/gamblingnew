cfceac367265f0e33cfd321c1765ec94a01fb0af
Prajwalbhutey09 <prajwalbhutey09@gmail.com>
2023-02-20 07:57:13 -0800

    code of adding stack

--- src/com/bridgelbz/GamblingMain.java
+++ src/com/bridgelbz/GamblingMain.java
@@ -3,5 +3,8 @@ package com.bridgelbz;
 public class GamblingMain {
     public static void main(String[] args) {
         System.out.println("WELCOME TO GAMBLING PROGRAM");
+
+        int stake = 100;
+        int bet = 1;
     }
 }
