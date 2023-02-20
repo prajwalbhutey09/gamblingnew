a80e02f3d6739629190bc094bc19402728f5d302
Prajwalbhutey09 <prajwalbhutey09@gmail.com>
2023-02-20 08:16:32 -0800

    code for calculativegambling

--- src/com/bridgelbz/GamblingMain.java
+++ src/com/bridgelbz/GamblingMain.java
@@ -5,12 +5,31 @@ public class GamblingMain {
         System.out.println("WELCOME TO GAMBLING PROGRAM");
 
         int stake = 100;
-        int bet = 1;
-        int random = (int)((Math.random() * 10) %2);
-        if(random == 1){
-            System.out.println("Gambler won the bet");
+        int betValue = 1;
+        int bet = 0;
+        int win = 0;
+        int loose = 0;
+
+        while (stake > 50 && stake < 150) {
+            int random = (int) ((Math.random() * 10) % 2);
+            if (random == 1) {
+                stake++;
+                win++;
+            } else {
+                stake--;
+                loose++;
+            }
+            bet++;
+        }
+
+        System.out.println("Gambler won the bet " + win + " Times");
+        System.out.println("Gambler looses the bet " + loose + " Times");
+        System.out.println("total bets made : " + bet);
+        if (stake == 150) {
+            System.out.println("gambler won for the day & takes " + stake + "$ home");
         } else {
-            System.out.println("Gambler looses the bet");
+            System.out.println("Gambler looses for the day & takes " + stake + "$ home");
         }
     }
 }
+
