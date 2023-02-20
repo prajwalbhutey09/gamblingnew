63c76c20ea9bfb82f007a46978085cd8af103fb6
Prajwalbhutey09 <prajwalbhutey09@gmail.com>
2023-02-20 08:23:51 -0800

    WinLoss

--- src/com/bridgelbz/GamblingMain.java
+++ src/com/bridgelbz/GamblingMain.java
@@ -9,27 +9,38 @@ public class GamblingMain {
         int bet = 0;
         int win = 0;
         int loose = 0;
+        int totalDays = 1;
+        int totalWin = 0;
+        int totalLoose =0;
 
-        while (stake > 50 && stake < 150) {
-            int random = (int) ((Math.random() * 10) % 2);
-            if (random == 1) {
-                stake++;
-                win++;
-            } else {
-                stake--;
-                loose++;
+        while(totalDays <= 20) {
+            stake = 100;
+
+            while (stake > 50 && stake < 150) {
+                int random = (int) ((Math.random() * 10) % 2);
+                if (random == 1) {
+                    stake++;
+                    win++;
+                } else {
+                    stake--;
+                    loose++;
+                }
+                bet++;
+
             }
-            bet++;
+            if (stake == 150) {
+                totalWin++;
+            } else {
+                totalLoose++;
+            }
+            totalDays++;
         }
 
-        System.out.println("Gambler won the bet " + win + " Times");
-        System.out.println("Gambler looses the bet " + loose + " Times");
+        System.out.println("Total Amount won by gambler in 20 days : " + totalWin*50 + "$");
+        System.out.println("Total Amount loosed by gambler in 20 days :" + totalLoose*50 + "$");
         System.out.println("total bets made : " + bet);
-        if (stake == 150) {
-            System.out.println("gambler won for the day & takes " + stake + "$ home");
-        } else {
-            System.out.println("Gambler looses for the day & takes " + stake + "$ home");
-        }
+
     }
 }
+
 
