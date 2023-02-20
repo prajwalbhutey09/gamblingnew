68794ee126507f2968259f7f7d6dbf15fd5e1540
Prajwalbhutey09 <prajwalbhutey09@gmail.com>
2023-02-20 08:04:29 -0800

    code of adding Makebet

--- src/com/bridgelbz/GamblingMain.java
+++ src/com/bridgelbz/GamblingMain.java
@@ -6,5 +6,11 @@ public class GamblingMain {
 
         int stake = 100;
         int bet = 1;
+        int random = (int)((Math.random() * 10) %2);
+        if(random == 1){
+            System.out.println("Gambler won the bet");
+        } else {
+            System.out.println("Gambler looses the bet");
+        }
     }
 }
